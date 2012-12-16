ae7d075601e312d28a4124860dcdc221f7564048
Demian Terentev <infusiastic@yahoo.com>
2012-12-16 11:24:38 +0800

    Version 42 (basic release without any obvious bugs) ready.

--- tunhong.rb
+++ tunhong.rb
@@ -3,38 +3,66 @@
 #
 # Tunhong parser
 #
-# A ruby library to markup Chinese and Tibetan strings inside the text
+# A library for Ruby 1.9 to mark Chinese and Tibetan strings up inside the text
 # written in another language.
 #
 # The name is taken after Dunhuang Caves in China, containing important
 # manuscripts in both Chinese and Tibetan.
 # 
-# Dunhuang spells 燉煌 in Chinese and ཏུན་ཧོང་ in Tibetan. We prefer the
-# Tibetan reading as the name for the parser.
+# Dunhuang spells 燉煌 in Chinese and ཏུན་ཧོང་ in Tibetan. The Tibetan reading
+# was chosen as the name for the parser.
 #
-# To illustrate the idea, the result of parsing the previous paragraph
-# could be:
+# To illustrate the idea, the result of parsing the first sentence of the
+# previous paragraph could be:
 #
-# Dunhuang spells <span lang="zh">燉煌</lang> in Chinese and <span lang="bo">
-# ཏུན་ཧོང་</span> in Tibetan. We prefer the Tibetan reading as the name
-# for the parser.
+# Dunhuang spells <span lang="zh">燉煌</span> in Chinese and <span lang="bo">
+# ཏུན་ཧོང་</span> in Tibetan.
 #
 # or it could be:
 #
-# Dunhuang spells [c]燉煌[ↄ] in Chinese and [t]ཏུན་ཧོང་[ʇ] in
-# Tibetan. We prefer the Tibetan reading as the name for the parser.
+# Dunhuang spells [c]燉煌[ↄ] in Chinese and [t]ཏུན་ཧོང་[ʇ] in Tibetan.
 #
 # depending on the user preference.
+#
+# Version 42
+#
+# Copyright © 2012 Demian Terentev
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is 
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 class Tunhong
 
-  def initialize(zh_start='<span lang="zh">', zh_end='</span>', bo_start='<span lang="zh">', bo_end='</span>')
+  # Initialize the parser
+  #
+  # `zh_start` and `zh_end` designate opening and closing tags for Chinese
+  # and `bo_start` and `bo_end` for Tibetan
+  #
+  # the default values are html `<span>` tags with the corresponding `lang`
+  # attributes
+  def initialize(zh_start='<span lang="zh">', zh_end='</span>', bo_start='<span lang="bo">', bo_end='</span>')
     @zh_start = zh_start
     @zh_end = zh_end
     @bo_start = bo_start
     @bo_end = bo_end
   end
   
+  # returns the copy of `str` where Tibetan or Chinese substrings are enclosed in corresponding tags 
   def parse(str)
     output = ""
     # mode designates the current language, it can be :chinese, :tibetan, or :other
@@ -44,32 +72,55 @@ class Tunhong
       # first try to see if the mode changes
       if chinese?(chr)
         mode = :chinese
+      elsif tibetan?(chr)
+        mode = :tibetan
       else
         mode = :other
       end
       # then decide if any tags are to input due to mode change
-      if mode!=old_mode # if no mode change occured
-        if mode==:chinese # if the text switched to chinese
-          output << @zh_start # then add a chinese opening tag
-        elsif old_mode==:chinese # if the text switched back from chinese
-          output << @zh_end # then add a chinese closing tag
+      if mode!=old_mode
+        # first the closing tags
+        if old_mode==:chinese
+          output << @zh_end
+        elsif old_mode==:tibetan
+          output << @bo_end
+        end
+        # then the opening tags
+        if mode==:chinese
+          output << @zh_start
+        elsif mode==:tibetan
+          output << @bo_start
         end
       end
       output << chr
+    end
+    # add closing tags at the end of the string
+    if mode==:chinese
+      output << @zh_end
+    elsif mode==:tibetan
+      output << @bo_end
     end
     output
   end
   
   private
   
+  # returns true if the character is in one of CJK Unicode ranges  
   def chinese?(chr)
-    chr=="c"
+    (0x4e00..0x9fff).include?(chr.ord) || # CJK Unified Ideographs
+    (0x3400..0x4dbf).include?(chr.ord) || # CJK Unified Ideographs Extension A
+    (0x20000..0x2a6df).include?(chr.ord) || # CJK Unified Ideographs Extension B
+    (0x2a700..0x2b73f).include?(chr.ord) || # CJK Unified Ideographs Extension C
+    (0x2b740..0x2b81f).include?(chr.ord) || # CJK Unified Ideographs Extension D
+    (0xf900..0xfaff).include?(chr.ord) # CJK Compatibility Ideographs
   end
   
+  # returns true if the character is in Tibetan Unicode range
   def tibetan?(chr)
-    chr=="t"
+    (0x0f00..0xfff).include?(chr.ord)
   end
 end
 
-puts "Hello, ཏུན་ཧོང་!"
-puts(Tunhong.new("[c]","[ↄ]","[t]","[ʇ]").parse("A test string of characccters."))
+# test_string = "Dunhuang spells 燉煌 in Chinese and ཏུན་ཧོང་ in Tibetan."
+# puts(Tunhong.new('[c]','[ↄ]','[t]','[ʇ]').parse(test_string))
+# puts(Tunhong.new().parse(test_string))
